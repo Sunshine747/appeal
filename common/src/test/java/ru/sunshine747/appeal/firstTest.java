be6119f766a6bb36cb7e1e9fba659f9a873f332b
Sunshine747 <denisdanilin58@gmail.com>
2018-06-24 22:50:01 +0300

    added testng;

---
+++ common/src/test/java/ru/sunshine747/appeal/firstTest.java
@@ -0,0 +1,12 @@
+package ru.sunshine747.appeal;
+
+import org.testng.annotations.Test;
+
+public class firstTest {
+
+    @Test
+    public void test() {
+        System.out.println("123");
+    }
+
+}
